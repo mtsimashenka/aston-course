f4afabbfe01dfe0c7f0ccd8649621fdf1a073aa0
Maksim Timoshenko <maksim-timoshenko-97@mail.ru>
2025-08-08 14:35:28 +0300

    add solution 2_5

--- src/main/java/lesson2_5/Main.java
+++ src/main/java/lesson2_5/Main.java
@@ -1,5 +1,6 @@
 package lesson2_5;
 
+import static lesson2_5.ArrayProcessor.demonstrateArrayIndexException;
 import static lesson2_5.ArrayProcessor.processArrayAndCalculateSum;
 
 public class Main {
@@ -28,6 +29,16 @@ public class Main {
                 {"7", "8", "9"}
         };
 
+        System.out.println("\n Test 2: Неправильный размер");
+        try {
+            int sum = processArrayAndCalculateSum(wrongSizeArray);
+            System.out.println("Сумма элементов: " + sum);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в ячейке [" + e.getRow() + "][" + e.getCol() + "]: " + e.getMessage());
+        }
+
         String[][] arrayWithText = {
                 {"1", "2", "3", "4"},
                 {"5", "abc", "7", "8"},
@@ -35,11 +46,33 @@ public class Main {
                 {"13", "14", "15", "16"}
         };
 
+        System.out.println("\n Test 3: Неправильные данные");
+        try {
+            int sum = processArrayAndCalculateSum(arrayWithText);
+            System.out.println("Сумма элементов: " + sum);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в ячейке [" + e.getRow() + "][" + e.getCol() + "]: " + e.getMessage());
+        }
+
         String[][] arrayWithEmpty = {
                 {"1", "2", "", "4"},
                 {"5", "6", "7", "8"},
                 {"9", "10", "11", "12"},
                 {"13", "14", "15", "16"}
         };
+
+        System.out.println("\n Test 4: Пустые значения");
+        try {
+            int sum = processArrayAndCalculateSum(arrayWithEmpty);
+            System.out.println("Сумма элементов: " + sum);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в ячейке [" + e.getRow() + "][" + e.getCol() + "]: " + e.getMessage());
+        }
+
+        demonstrateArrayIndexException();
     }
 }
